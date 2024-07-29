b18dab1f353365379d3f8b86bc4bd337ebd81400
olikelly00 <oliverkelly1995@hotmail.co.uk>
2024-07-29 10:56:27 +0100

    built handler function for get all recipes

--- app.js
+++ app.js
@@ -17,3 +17,13 @@ app.use(express.json());
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+//This handler function will return all the recipes we have when called
+app.get('/recipes', (req, res) => {
+  try {
+    res.status(200).send("test string")
+} catch (e) {
+  res.status(404).send("that resource does not exist")
+  console.error(e)
+}
+});
