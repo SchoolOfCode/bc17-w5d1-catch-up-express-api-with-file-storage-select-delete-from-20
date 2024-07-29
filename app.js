7f1da73996849406199322655ef0517c1f862c17
Ryan <ryanshaunsmith@gmail.com>
2024-07-29 11:20:03 +0100

    added initial post request handler

--- app.js
+++ app.js
@@ -18,21 +18,46 @@ app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
-//This handler function will return all the recipes we have when called
+// This handler function will return all the recipes we have when called
 app.get('/recipes', (req, res) => {
   try {
     res.status(200).send("test string")
-} catch (e) {
-  res.status(404).send("that resource does not exist")
-  console.error(e)
-}
+  } catch (e) {
+    res.status(404).send("that resource does not exist")
+    console.error(e)
+  }
 });
 
-app.get ('/recipes/123', (req, res) => {
+// This handler function will return a specific recipe
+app.get('/recipes/123', (req, res) => {
   try {
     res.status(200).send("test string 123")
-} catch (e) {
-  console.error(e)
-  res.status(404).send("that resource does not exist 123")
-}
+  } catch (e) {
+    console.error(e)
+    res.status(404).send("that resource does not exist 123")
+  }
+});
+
+// {
+//   "id": "4c848d48-b81e-4d6f-b45d-7b3090f4f8ej",
+//   "title": "Avo on Toast",
+//   "ingredients": ["150g of Avo", "150g of butter", "150g of toast"],
+//   "instructions": "Don't use butter, wait 2 seconds to allow slight melting. Then follow with the toast. Swish around for 10-15 seconds to allow even coating of butter on the toast. Then add the beans, slowly.\n  \n    Season to taste.",
+//   "image": "https://natashaskitchen.com/wp-content/uploads/2019/04/Best-Burger-4-500x375.jpg"
+// }
+
+// This handler function will return all the recipes we have when called
+app.post('/recipes', (req, res) => {
+  const newRecipe = req.body;
+  try {
+    res.status(201).json({
+      'success': true,
+      'payload': newRecipe
+    });
+  } catch (e) {
+    res.status(300).json({
+      'success': false,
+      'payload': null
+    });
+  };
 });
